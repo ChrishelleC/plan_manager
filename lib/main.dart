42388aec648ff9e638bf934b862369e9c751aaff
ChrishelleC <culmerchrishelle12@gmail.com>
2025-03-09 21:27:33 -0400

    Another change

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class _PlanManagerScreenState extends State<PlanManagerScreen> {
     });
   }
 
-  // Toggle completion status of a plan
+  // To toggle completion status of a plan
   void _toggleComplete(int index) {
     setState(() {
       plans[index].isCompleted = !plans[index].isCompleted;
